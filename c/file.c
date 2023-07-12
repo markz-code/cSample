d8636b7819a738e36dad3a08731b2b64bcb080f6
zpl <13938499402.163.com>
2023-07-12 15:46:30 +0000

    修改2

--- c/file.c
+++ c/file.c
@@ -5,7 +5,7 @@ int main()
     FILE *fp = NULL;
     fp = fopen("./temp.txt","w+");
     fprintf(fp, "hello world\n");
-    fputs("this is test\n",fp);
+    fputs("this is test2\n",fp);
     fclose(fp);
     return 0;
 }

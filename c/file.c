ea08a7a8ab42ef19093f0a4ea9bd62d4961fb606
zpl <13938499402.163.com>
2023-07-12 15:43:38 +0000

    修改函数

--- c/file.c
+++ c/file.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-void main() 
+int main() 
 {
     FILE *fp = NULL;
     fp = fopen("./temp.txt","w+");
     fprintf(fp, "hello world\n");
     fputs("this is test\n",fp);
     fclose(fp);
+    return 0;
 }
